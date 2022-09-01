e1020ebd7970851ed8f509583fe901d141230b42
Confidence Madiope <confidencem97@gmail.com>
2022-09-01 15:42:56 +0200

    ADD: fetch api

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = '71544f1c2a2ee7119c4899009c6843b3'
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
